9cded4ac06f59e797fe65804c02009a94621b4db
Lcfz01 <lmze001@gmail.com>
2024-06-17 17:17:55 -0600

    finished calculator

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,7 @@
+from functools import partial
+
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QLineEdit, QWidget, QGridLayout
+from PySide6.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QLineEdit, QWidget, QGridLayout, QPushButton
 
 
 class Calculator(QMainWindow):
@@ -14,6 +16,7 @@ class Calculator(QMainWindow):
         #visuals
         self._createDisplay()
         self._createButtons()
+        self._connButtons()
 
     def _createDisplay(self):
         self._input = QLineEdit()
@@ -23,10 +26,10 @@ class Calculator(QMainWindow):
         self.mainLayout.addWidget(self._input)
 
     def _createButtons(self):
-        self.botones = {}
+        self.calcButtons = {}
         self.layoutButtons = QGridLayout()
         #text-position
-        self.botones={
+        self.calcButtons={
             '7':(0,0),
             '8': (0, 1),
             '9': (0, 2),
@@ -45,6 +48,39 @@ class Calculator(QMainWindow):
             '+': (3, 3),
             '=': (3, 4),
         }
+
+        #add buttons to dict
+        for text,pos in self.calcButtons.items():
+            self.calcButtons[text] = QPushButton(text)
+            self.calcButtons[text].setFixedSize(40,40)
+            self.layoutButtons.addWidget(self.calcButtons[text],pos[0],pos[1])
+
+    def _connButtons(self):
+        def _expresion(text):
+            self._input.setText(self._input.text() + text)
+        def _c():
+            self._input.clear()
+        def _result():
+            try:
+                self._input.setText(str(eval(self._input.text()))) #wow magic
+            except Exception as e:
+                self._input.setText("ERROR")
+
+
+
+
+        for text,button in self.calcButtons.items():
+            if text == 'C':
+                button.clicked.connect(_c)
+            elif text == '=':
+                button.clicked.connect(_result)
+            else:
+                button.clicked.connect(partial(_expresion,text))
+
+
+
+        self.mainLayout.addLayout(self.layoutButtons)
+
 
 if __name__ == "__main__":
     app = QApplication()
